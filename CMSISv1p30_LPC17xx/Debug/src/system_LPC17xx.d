27989c6252f47edaf2002d8419f86f5d0932c49c
nlv14991 <sami.masad@stericsson.com>
2012-08-08 10:39:57 +0200

    optimize compiler

--- CMSISv1p30_LPC17xx/Debug/src/system_LPC17xx.d
+++ CMSISv1p30_LPC17xx/Debug/src/system_LPC17xx.d
@@ -1,10 +1,10 @@
 src/system_LPC17xx.d src/system_LPC17xx.o: ../src/system_LPC17xx.c \
-  C:\Documents\ and\ Settings\nlv14991\My\ Documents\lpcxpresso_3.5\workspace\CMSISv1p30_LPC17xx\inc/LPC17xx.h \
-  C:\Documents\ and\ Settings\nlv14991\My\ Documents\lpcxpresso_3.5\workspace\CMSISv1p30_LPC17xx\inc/core_cm3.h \
-  C:\Documents\ and\ Settings\nlv14991\My\ Documents\lpcxpresso_3.5\workspace\CMSISv1p30_LPC17xx\inc/system_LPC17xx.h
+ /home/nlv14991/workspaceLPC/STE-LPC1768-/CMSISv1p30_LPC17xx/inc/LPC17xx.h \
+ /home/nlv14991/workspaceLPC/STE-LPC1768-/CMSISv1p30_LPC17xx/inc/core_cm3.h \
+ /home/nlv14991/workspaceLPC/STE-LPC1768-/CMSISv1p30_LPC17xx/inc/system_LPC17xx.h
 
-C:\Documents\ and\ Settings\nlv14991\My\ Documents\lpcxpresso_3.5\workspace\CMSISv1p30_LPC17xx\inc/LPC17xx.h:
+/home/nlv14991/workspaceLPC/STE-LPC1768-/CMSISv1p30_LPC17xx/inc/LPC17xx.h:
 
-C:\Documents\ and\ Settings\nlv14991\My\ Documents\lpcxpresso_3.5\workspace\CMSISv1p30_LPC17xx\inc/core_cm3.h:
+/home/nlv14991/workspaceLPC/STE-LPC1768-/CMSISv1p30_LPC17xx/inc/core_cm3.h:
 
-C:\Documents\ and\ Settings\nlv14991\My\ Documents\lpcxpresso_3.5\workspace\CMSISv1p30_LPC17xx\inc/system_LPC17xx.h:
+/home/nlv14991/workspaceLPC/STE-LPC1768-/CMSISv1p30_LPC17xx/inc/system_LPC17xx.h:
